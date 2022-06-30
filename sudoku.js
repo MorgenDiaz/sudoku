c42cf2fa9fab350559f3b6d833b8e34f61b4e2d5
Morgen Diaz <morgen@webslinger.works>
2022-06-29 18:08:26 -1000

    Created helper functions to select a subsection at given coordinates and to validate a subsection.

--- sudoku.js
+++ sudoku.js
@@ -12,10 +12,41 @@ let puzzle = [
   [3, 2, 8, 1, 9, 6, 5, 4, 7],
 ];
 
-function getRow(puzzle, row) {
+function getRow(puzzle, row = 0) {
   return puzzle[row];
 }
 
-function getColumn(puzzle, column) {
+function getColumn(puzzle, column = 0) {
   return puzzle.slice(0, puzzle.length).map((i) => i[column]);
 }
+
+function coordinatesExceedBounds(startRow, startCol) {
+  return startRow >= puzzle[0].length || startCol >= puzzle.length;
+}
+
+function getSection(puzzle, x = 0, y = 0) {
+  let startRow = y * 3;
+  let endRow = startRow + 3;
+  let startCol = x * 3;
+  let endCol = startCol + 3;
+
+  if (coordinatesExceedBounds(startRow, startCol)) {
+    throw RangeError("Section coordinates are out of bounds.");
+  }
+
+  return puzzle
+    .slice(startRow, endRow)
+    .map((i) => i.slice(startCol, endCol))
+    .flat();
+}
+
+function contains1to9(subsection) {
+  const validSubsection = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  return (
+    validSubsection.every((i) => subsection.includes(i)) &&
+    subsection.length === validSubsection.length
+  );
+}
+
+console.log(contains1to9([1, 2, 3, 4, 5, 6, 7, 8, 9]));
